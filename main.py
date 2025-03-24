65fed7b94334adef2057afa1b133b2e6cc2a0a1d
aswnvm <aswnvm@gmail.com>
2025-03-24 13:56:23 +0530

    enhanced the logic

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from random import randint
 import random
 import os
 import json
+import re
 
 
 def main():
@@ -21,22 +22,23 @@ def main():
         repo = git.Repo(local_rep_path)
     print(local_rep_path)
 
-    file_list(local_rep_path)
+    sel_file = file_list(local_rep_path)
     days = int(input("Enter the num of days to commit: "))
 
     # file_count = int(input("Enter the num of files to be modified: "))
 
     # files = []
     # file_name =os.path.join(local_rep_path, str(input(f"{i+1}.Enter the file name with extension: ")))
-    
+
     # file_check(files)
 
     for day in range(0, days):
         date = (datetime.today() - timedelta(days=day)).strftime("%Y-%m-%d %H:%M:%S")
         for i in range(0, randint(1, 5)):
             print(date)
-            commit_n_push(local_rep_path, repo, date)
+            commit_n_push(os.path.join(local_rep_path, sel_file), repo, date)
     print("Commits done successfully!")
+
 
 def file_list(dpath):
     files = [f for f in os.listdir(dpath) if os.path.isfile(os.path.join(dpath, f))]
@@ -47,52 +49,74 @@ def file_list(dpath):
         for i, file in enumerate(files, start=1):
             print(f"{i}. {file}")
 
+    try:
         choice = int(input("\nChooose the file you want to modify: ")) - 1
 
         if 0 <= choice < len(files):
             sel_file = files[choice]
             print(sel_file)
+            return files[choice]
         else:
             print("\nInvalid selection.")
-    return sel_file
-
-# def file_check(fn):
-
-#     if os.path.exists(fn):
-#         print(f"file {fn} exists")
-#     else:
-#         print(f"file {fn} does not exist")    
-
-#         if os.path.splitext(f"r{file}")[1] == ".py":
-#             print(f"file {file} is a python file")
-#         else:
-#             print(f"file {file} is not a python file")
-        
-#     return os.path.splitext(f"r{file}")[1]
+            return None
+    except ValueError:
+        print("\nInvalid input. Exiting...")
+        return None
 
 
-# def load_file_content(file_path):
-#     with open(file_path, "r") as f:
-#         return [line.strip() for line in f.readlines() if line.strip()]
+def json_load(jpath):
+    with open(jpath, "r") as f:
+        data = json.load(f)
+    keys = list(data.keys())
+    idx = random.randint(0, len(keys) - 1)
+    dat = data[keys[idx]]
+    return dat
+
+
+def file_modifier(fpath, new_content, flag):
+    with open(fpath, "r") as f:
+        content = f.read()
+    pattern = r'print\((["\'])(.*?)\1\)' if flag else r"[^.!?]+[.!?]"
+
+    matches = re.findall(pattern, content)
+
+    if matches:
+        old_text = random.choice(matches)[1]
+        if flag:
+            new_cont = new_content.replace("\\", "\\\\")
+            change = re.sub(
+                rf'print\((["\']){re.escape(old_text)}\1\)',
+                f"print({matches[0][0]}{new_cont}{matches[0][0]})",
+                content,
+                count=1,
+            )
+        else:
+            change = content.replace(random.choice(matches), new_content, 1)
+        with open(fpath, "w") as f:
+            f.write(change)
+        return True
+
+    return False
 
 
 def commit_n_push(fpath, repo, date):
-    
-    readme_cont = load_file_content("readme_contents.txt")
-    commit_msg = load_file_content("commit_messages.txt")
 
-    random_readme_cont = random.choice(readme_cont)
-    random_commit_msg = random.choice(commit_msg)
-    # with open(os.path.join(local_rep_path + "/README.md"), "w") as f:
-    #     # f.write(f"This is a README file for the repository. Updated at {date}\n")
-    #     f.write(f"{random_readme_cont} {date}\n")
-    # repo.index.add([os.path.join(local_rep_path + "/README.md")])
+    if os.path.splitext(fpath)[1] == ".py":
+        flag = True
+        json_f = "content_code.json"
+    else:
+        flag = False
+        json_f = "content_readme.json"
 
-    with open(fpath, "w") as f:
-        f.write()
-    repo.index.add([fpath])
-    repo.index.commit(random_commit_msg, author_date=date, commit_date=date)
-    repo.remotes.origin.push()
+    random_cont = json_load(json_f)
+    modified = file_modifier(fpath, random_cont["text"], flag)
+
+    if modified:
+        repo.index.add([fpath])
+        repo.index.commit(random_cont["commit_msg"], author_date=date, commit_date=date)
+        repo.remotes.origin.push()
+    else:
+        print("No changes made. Skipping commit.")
 
 
 if __name__ == "__main__":
